f10ac56316be7b5ee8e973192f607ce458875aa3
bibekkunwar <bibekkunwar0@gmail.com>
2023-05-22 11:54:16 +0545

    compnents added and routed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { HomeBodyComponent } from './homeBody/home-body.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
-    path : 'about',
-    component : AboutComponent,
-  },
-  {
-    path : 'homeBody',
+    path : 'home',
     component : HomeBodyComponent,
   },
   {
-    path : '', redirectTo : '/home', pathMatch : 'full'
+    path : '', redirectTo : 'about', pathMatch : 'full'
+  },
+  {
+    path : 'about',
+    component : AboutComponent
+  },
+  {
+    path : 'contact',
+    component: ContactComponent
   }
+
+
 ];
 
 @NgModule({
